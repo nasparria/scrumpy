eeb7270eb926bc0e5871460e7b92effa6649ea02
nicolas asparria <nasparria@gmail.com>
2022-06-07 13:47:38 -0500

    Initial commit

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,23 +1,18 @@
 from django import forms
 
-try:
-    from recipes.models import Recipe
 
-    class RecipeForm(forms.ModelForm):
-        class Meta:
-            model = Recipe
-            fields = [
-                "name",
-                "author",
-                "description",
-                "image",
+from recipes.models import Recipe, Rating
+
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [
+            "name",
+            "author",
+            "description",
+            "image",
             ]
-
-except Exception:
-    pass
-
-
-from recipes.models import Rating
 
 
 class RatingForm(forms.ModelForm):
